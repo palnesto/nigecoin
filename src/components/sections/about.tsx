7910f31e1672bc375f2ad5602c0c6eec8c10bfe5
saranshkhulbe7 <saranshkhulbe7@gmail.com>
2024-11-29 23:18:02 +0530

    graph desktop view

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -135,7 +135,7 @@ export function AboutSection() {
           </p>
         </div>
       </div> */}
-      <div className="bg-blue md:py-40">
+      <div className="bg-blue">
         <div className="relative bg-green grid grid-cols-5 w-full py-14 px-20">
           <div className=" col-span-3 w-full relative">
             <div className="absolute aspect-square h-[50rem] z-50 -top-60 ">
@@ -231,13 +231,9 @@ export function AboutSection() {
         </div>
       </div> */}
 
-      <div className="bg-blue relative overflow-x-auto mb-24 pt-40">
+      <div className="bg-blue relative overflow-x-auto scrollbar-x mb-24 pt-20">
         <div className="w-[300vw] sm:w-[200vw] md:w-full">
-          <img
-            src="/public/graph.png"
-            alt=""
-            className="w-full border border-white"
-          />
+          <img src="/public/graph.png" alt="" className="w-full" />
         </div>
         {/* till start of md only */}
         <div className="absolute bottom-7 w-full h-10 md:hidden">
@@ -319,7 +315,7 @@ export function AboutSection() {
             {/* bar 4 */}
             <div className="absolute bottom-0 ml-[231vw] w-[68vw] h-[140vw] sm:h-[100vw] sm:ml-[154vw] sm:w-[46vw] group">
               <div className="relative h-full w-full sm:group-hover:cursor-pointer">
-                <div className="opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 ease-in group-hover:transition-opacity group-hover:duration-500 origin-center radial-gradient-purple aspect-square h-20 rounded-full absolute left-[-10%] top-[-10%]" />
+                <div className="opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 ease-in group-hover:transition-opacity group-hover:duration-300 origin-center radial-gradient-purple aspect-square h-20 rounded-full absolute left-[-10%] top-[-10%]" />
                 {/* title + button */}
                 <div className="absolute w-[80%] bottom-[93vw] sm:bottom-[62vw] ml-[5%] flex flex-col gap-[2vw] sm:gap-[3vw] text-white sm:text-white/70 sm:group-hover:text-white">
                   <h2 className="font-bold text-[4vw] sm:text-[3vw]">
@@ -333,6 +329,104 @@ export function AboutSection() {
                 </div>
                 {/* eagle image */}
                 <div className="w-[40%] absolute bottom-[90vw] sm:bottom-[60vw] right-[-5%] opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <img
+                    src="/public/character.png"
+                    alt="eagle"
+                    className="h-full w-full object-contain"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* after md */}
+        <div className="hidden absolute inset-0 bg-transparent z-10 md:flex items-end pb-[1.8vw]">
+          <div className="relative h-20 w-full">
+            {/* bar 1 */}
+            <div className="w-[24vw] max-container:w-[475.2px] ml-[5.4vw] max-container:ml-[106.92px] h-[26vw] max-container:h-[514.8px] absolute bottom-0 group">
+              <div className="h-full w-full bg-transparent relative group-hover:cursor-pointer">
+                <Gradient />
+                <div className="absolute w-[60%] bg-transparent bottom-[35%] ml-[5%] flex flex-col gap-[1vw]  text-white/70 group-hover:text-white">
+                  <h2 className="font-bold text-[1.3vw]">
+                    Phase - 1 <br /> Foundation & <br /> Launch (Q3 2024)
+                  </h2>
+                  <div className="border-white border rounded-full uppercase text-center py-[0.5vw] text-[0.6vw] w-fit px-[1.2vw] flex justify-center items-center gap-2 hover:cursor-pointer hover:bg-white/10 transition-all duration-150">
+                    show more
+                    <BiChevronDown />
+                  </div>
+                </div>
+                <div className="w-[40%] absolute bottom-[32%] right-[-5%] opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <img
+                    src="/public/character.png"
+                    alt="eagle"
+                    className="h-full w-full object-contain"
+                  />
+                </div>
+              </div>
+            </div>
+            {/* bar 2 */}
+            <div className="w-[22vw] max-container:w-[435.6px] ml-[30.5vw] max-container:ml-[603.9px] h-[30vw] max-container:h-[594px] absolute bottom-0 group">
+              <div className="h-full w-full bg-transparent relative group-hover:cursor-pointer">
+                <Gradient />
+                <div className="absolute w-[60%] bg-transparent bottom-[47%] ml-[5%] flex flex-col gap-[1vw] text-white/70 group-hover:text-white">
+                  <h2 className="font-bold text-[1.3vw]">
+                    Phase - 2 <br /> Growth & Expansion <br /> (Q4 2024 - Q1
+                    2025)
+                  </h2>
+                  <div className="border-white border rounded-full uppercase text-center py-[0.5vw] text-[0.6vw] w-fit px-[1.2vw] flex justify-center items-center gap-2 hover:cursor-pointer hover:bg-white/10 transition-all duration-150">
+                    show more
+                    <BiChevronDown />
+                  </div>
+                </div>
+                <div className="w-[40%] absolute bottom-[45%] right-[-5%] opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <img
+                    src="/public/character.png"
+                    alt="eagle"
+                    className="h-full w-full object-contain"
+                  />
+                </div>
+              </div>
+            </div>
+            {/* bar 3 */}
+            <div className="w-[22vw] max-container:w-[435.6px] ml-[54vw] max-container:ml-[1069.2px] h-[35vw] max-container:h-[693px] absolute bottom-0 group">
+              <div className="h-full w-full bg-transparent relative group-hover:cursor-pointer">
+                <Gradient />
+                <div className="absolute w-[60%] bg-transparent bottom-[58%] ml-[5%] flex flex-col gap-[1vw] text-white/70 group-hover:text-white">
+                  <h2 className="font-bold text-[1.3vw]">
+                    Phase - 3 <br /> Platform Launch <br />& Ecosystem
+                    Integration <br /> (Q2 2025 -Q4 2025)
+                  </h2>
+                  <div className="border-white border rounded-full uppercase text-center py-[0.5vw] text-[0.6vw] w-fit px-[1.2vw] flex justify-center items-center gap-2 hover:cursor-pointer hover:bg-white/10 transition-all duration-150">
+                    show more
+                    <BiChevronDown />
+                  </div>
+                </div>
+                <div className="w-[40%] absolute bottom-[57%] right-[-5%] opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <img
+                    src="/public/character.png"
+                    alt="eagle"
+                    className="h-full w-full object-contain"
+                  />
+                </div>
+              </div>
+            </div>
+            {/* bar 4 */}
+            <div className=" w-[23vw] max-container:w-[455.4px] ml-[77vw] max-container:ml-[1524.6px] h-[40vw] max-container:h-[792px] absolute bottom-0 group">
+              <div className="h-full w-full bg-transparent relative group-hover:cursor-pointer">
+                <div className="opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 ease-in group-hover:transition-opacity group-hover:duration-300 origin-center radial-gradient-purple aspect-square h-20 rounded-full absolute left-[-10%] top-[-25%]" />
+                <div className="absolute w-[60%] bg-transparent bottom-[78%] ml-[5%] flex flex-col gap-[1vw] text-white/70 group-hover:text-white">
+                  <h2 className="font-bold text-[1.3vw]">
+                    Phase - 4 <br /> Long-Term Vision & <br /> Sustainability{" "}
+                    <br />
+                    (2026 & Beyond)
+                  </h2>
+                  <div className="border-white border rounded-full uppercase text-center py-[0.5vw] text-[0.6vw] w-fit px-[1.2vw] flex justify-center items-center gap-2 hover:cursor-pointer hover:bg-white/10 transition-all duration-150">
+                    show more
+                    <BiChevronDown />
+                  </div>
+                </div>
+                <div className="w-[40%] absolute bottom-[76%] right-[0%] opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                   <img
                     src="/public/character.png"
                     alt="eagle"
@@ -405,5 +499,5 @@ export function AboutSection() {
 }
 
 const Gradient = () => (
-  <div className="opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 ease-in group-hover:transition-opacity group-hover:duration-500 origin-center radial-gradient-purple aspect-square h-20 rounded-full absolute left-[-10%]" />
+  <div className="opacity-0 group-hover:opacity-100 scale-50 group-hover:scale-100 ease-in group-hover:transition-opacity group-hover:duration-300 origin-center radial-gradient-purple aspect-square h-20 rounded-full absolute left-[-10%]" />
 );
