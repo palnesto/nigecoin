e9e84b2f6bfe97d6bbb30a81148f6dc17853c5ba
VIDHI-tech <gaydhanevidhi@gmail.com>
2024-12-07 15:39:16 +0530

    img path correction

--- src/components/sections/reward.tsx
+++ src/components/sections/reward.tsx
@@ -7,9 +7,7 @@ export const RewardSection = () => {
           className="absolute top-0 left-0 xs:w-72 md:w-[65%] lg:w-72 xl:w-96 2xl:w-[450px]" 
           alt="Telegram Icon" 
         />
-        <div className="flex flex-col text-center lg:text-left lg:flex-row justify-between items-center ">
-        {/* items-center lg:gap-10 sm:pt-96 lg:pt-32  */}
-          {/* Left Section */}
+        <div className="flex flex-col text-center lg:text-left lg:flex-row justify-between items-center "> 
             <div className="xs:pt-48 sm:pt-96 lg:pt-28 lg:text-left lg:pl-24 2xl:pt-10">
               <h1 className="benz-grotesk font-bold pb-7 sm:pb-12">
                 <span className="block text-[#F7F6E1] text-[6vw] lg:text-[35px] xl:text-[45px] 2xl:text-[50px]">Collect Coins with</span>
@@ -19,8 +17,7 @@ export const RewardSection = () => {
                 Let's Begin
               </button>
             </div>
-  
-          {/* Right Section */}
+   
           <div className="bg-purple-gradient xs:pt-20 sm:w-3/4 md:pt-24  md:w-[80vw] lg:w-[50vw] xl:w-[50vw]">
             <img 
               src="/public/reward.png" 
@@ -29,10 +26,9 @@ export const RewardSection = () => {
             />
           </div>
         </div>
-  
-        {/* Decorative Image */}
+   
         <img 
-          src="/public/Mask group copy.png" 
+          src="/Mask group copy.png" 
           className="absolute hidden sm:block -left-10 -bottom-28 z-10 h-40 sm:h-52 transform scale-x-[-1]" 
           alt="Decorative" 
         />
